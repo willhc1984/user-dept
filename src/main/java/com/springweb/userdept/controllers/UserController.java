85d6b9147e01b71f2175d0dba36059317ecd673d
unknown <willhc1984@gmail.com>
2022-05-19 16:20:40 -0300

    Criação Repository e Controllers

--- src/main/java/com/springweb/userdept/controllers/UserController.java
+++ src/main/java/com/springweb/userdept/controllers/UserController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +24,18 @@ public class UserController {
 	public List<User> findAll(){
 		List<User> users = repository.findAll();
 		return users;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public User findById(@PathVariable Long id) {
+		User user = repository.findById(id).get();
+		return user;
+	}
+	
+	@PostMapping
+	public User insert(@RequestBody User user) {
+		User result = repository.save(user);
+		return result;
 	}
 	
 }
